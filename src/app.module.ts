12d0dae83de2244cf056acfe69bc3e9a0bd46629
michellopes338 <lopesmichel489@gmail.com>
2022-10-06 01:55:58 -0300

    inicio do projeto

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
